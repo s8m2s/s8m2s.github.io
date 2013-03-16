5c257fab5dfe51ecfd8f7ae32c4b30410b7364cb
Scott Stamile <scottstamile@gmail.com>
2013-03-16 12:40:08 -0400

    moved site into www dir

--- config.rb
+++ config.rb
@@ -2,11 +2,11 @@
 # require 'susy'
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "css"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "js"
+http_path = "www/"
+css_dir = "www/css"
+sass_dir = "www/sass"
+images_dir = "www/images"
+javascripts_dir = "www/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
