d1e90b53837f9cb81863f57194e903f1835a8405
Scott Stamile <scottstamile@gmail.com>
2013-12-07 23:16:32 -0500

    added smooth scrolling

--- javascripts/main.js
+++ javascripts/main.js
@@ -19,6 +19,21 @@ if (Modernizr.flexbox) {
 
 };
 
+// Smooth scroll to any anchor
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
 
 
 
