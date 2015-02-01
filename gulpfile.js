61c83aa8fbd5f57fd0d2f4c4ed9e0020aa06b212
Scott Stamile <scottstamile@gmail.com>
2015-02-01 00:19:14 -0500

    creating a serve task

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 var gulp = require('gulp'),
 	uglify = require('gulp-uglify'),
-	rename = require('gulp-rename');
+	rename = require('gulp-rename'),
+	jshint = require('gulp-jshint'),
+	autoprefixer = require('gulp-autoprefixer'),
+	spawn = require('child_process').spawn;
+
+gulp.task('lint', function(){
+
+	gulp.src(['javascripts/*.js','!javascripts/*.min.js'])
+	.pipe(jshint());
+
+});
 
 gulp.task('min', function(){
-	
-	// var minfilter = filter(['**','!javascripts/min'])
 
 	gulp.src(['javascripts/**/*.js','!javascripts/**/*.min.js'])
 	.pipe(uglify())
@@ -12,3 +20,18 @@ gulp.task('min', function(){
 	.pipe(gulp.dest('javascripts'));
 
 });
+
+gulp.task('jekyll', function(gulpCallBack){
+	var jekyll = spawn('jekyll', ['serve', '--config', '_config.yml','--trace'], {stdio: 'inherit'});
+	jekyll.on('exit', gulpCallBack);
+});
+
+gulp.task('watch', function(){
+	gulp.watch(['javascripts/**/*.js','!javascripts/**/*.min.js'], ['min']);
+});
+
+gulp.task('default', ['lint','min']);
+
+gulp.task('serve', ['watch','jekyll']);
+
+gulp.task('build', ['lint','min','jekyll']);
